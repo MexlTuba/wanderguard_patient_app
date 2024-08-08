5c3e6a75bc5f33a13148e2bdf4715ea3293ee6e2
MathewAlcoseba <mathewalcoseba25@gmail.com>
2024-08-08 15:00:42 +0800

    force call added

--- lib/services/zegocloud_service.dart
+++ lib/services/zegocloud_service.dart
@@ -1,12 +1,14 @@
-// lib/zego_service_helper.dart
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:wanderguard_patient_app/screens/call_screen.dart';
 import 'package:zego_uikit_prebuilt_call/zego_uikit_prebuilt_call.dart';
 import 'package:zego_uikit_signaling_plugin/zego_uikit_signaling_plugin.dart';
-import 'package:flutter/material.dart';
 
 class ZegoServiceHelper {
   final String patientAcctId;
   final String patientName;
   final GlobalKey<NavigatorState> navigatorKey;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
   ZegoServiceHelper({
     required this.patientAcctId,
@@ -15,6 +17,8 @@ class ZegoServiceHelper {
   });
 
   void initialize() {
+    print(
+        'Initializing Zego for Patient with ID: $patientAcctId, Name: $patientName');
     ZegoUIKitPrebuiltCallInvitationService().init(
       appID: 629459745,
       appSign:
@@ -23,9 +27,42 @@ class ZegoServiceHelper {
       userName: patientName,
       plugins: [ZegoUIKitSignalingPlugin()],
     );
+
+    _listenForIncomingCalls();
   }
 
   void deinitialize() {
+    print('Deinitializing Zego for Patient');
     ZegoUIKitPrebuiltCallInvitationService().uninit();
+  }
+
+  void _listenForIncomingCalls() {
+    _firestore
+        .collection('custom_calls')
+        .where('receiverId', isEqualTo: patientAcctId)
+        .snapshots()
+        .listen((snapshot) {
+      for (var doc in snapshot.docs) {
+        if (doc['status'] == 'calling') {
+          _acceptIncomingCall(doc.id, doc['callerId'], doc['callerName']);
+        }
+      }
+    });
+  }
+
+  void _acceptIncomingCall(String callId, String callerId, String callerName) {
+    _firestore.collection('custom_calls').doc(callId).update({
+      'status': 'accepted',
+    });
+
+    navigatorKey.currentState?.push(
+      MaterialPageRoute(
+        builder: (context) => CallScreen(
+          currentUserId: patientAcctId,
+          userId: callerId,
+          userName: callerName,
+        ),
+      ),
+    );
   }
 }
